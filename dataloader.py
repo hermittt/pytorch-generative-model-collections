f4b7e434446323d8940ce92cdc88dc7961b7538d
hermittt <499655727@qq.com>
2023-05-24 09:19:00 +0000

    form colab

--- dataloader.py
+++ dataloader.py
@@ -3,9 +3,10 @@ from torchvision import datasets, transforms
 
 def dataloader(dataset, input_size, batch_size, split='train'):
     transform = transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor(), transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+    transform2 = transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor(), transforms.Normalize(mean=(0.5), std=(0.5))])
     if dataset == 'mnist':
         data_loader = DataLoader(
-            datasets.MNIST('data/mnist', train=True, download=True, transform=transform),
+            datasets.MNIST('data/mnist', train=True, download=True, transform=transform2),
             batch_size=batch_size, shuffle=True)
     elif dataset == 'fashion-mnist':
         data_loader = DataLoader(
